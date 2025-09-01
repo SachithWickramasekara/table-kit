5ae774769375dabfd692a86212fa5d2c962426c2
Sachith Wickramasekara <101526693+SachithWickramasekara@users.noreply.github.com>
2025-09-02 01:19:23 +0530

    Add test dependencies and test import file

---
+++ test-import.tsx
@@ -0,0 +1,12 @@
+import { TableKit } from '@sachithw/table-kit';
+import '@sachithw/table-kit/styles';
+
+function App() {
+  return (
+    <>
+      <TableKit />
+    </>
+  )
+}
+
+export default App
